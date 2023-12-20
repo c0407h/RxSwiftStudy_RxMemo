b2a65d4d673e33fc93ba09536b032bebddc8340e
c0407h <67133244+c0407h@users.noreply.github.com>
2023-12-21 00:18:32 +0900

    메모보기화면에서 사용하는 뷰모델 구현

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
@@ -27,7 +29,27 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     }
 
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        
+        viewModel.contents
+            .bind(to: contentTableView.rx.items) { tableView, row, value in
+                switch row {
+                case 0:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                case 1:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
     }
     
 }
--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+
+
+class MemoDetailViewModel: CommonViewModel {
+    let memo: Memo
+    
+    private var formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "ko_kr")
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+       return f
+    }()
+    
+    
+    //첫번째셀에는 메모 표시 두번째 셀에는 날짜 표시
+    //테이블뷰에 데이터 표시를 위해 Observable과 바인딩
+    //테이블뷰에 표시할 데이터는 문자열 두개 따라서 문자열 배열로 방출
+    //BehaviorSubject를 사용한 이유 -> 메모 보기화면에서 메모편집버튼을 누르고 메모 편집하고 다시 메모 보기 화면으로 돌아오면
+    //편집된 내용이 반영되어야 하는데 이때 새로운 문자열 배열을 방출해야함
+    //일반 Observable로 선언하면 이게불가능 하기때문에 BehaviorSubject로 선언
+    var contents: BehaviorSubject<[String]>
+    
+    init(memo: Memo, title: String, sceneCoordinator: SceneCoordinator, storage: MemoryStorage) {
+        self.memo = memo
+        contents = BehaviorSubject<[String]>(value: [
+            memo.content,
+            formatter.string(from: memo.insertDate)
+        ])
+        super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+    }
     
 }

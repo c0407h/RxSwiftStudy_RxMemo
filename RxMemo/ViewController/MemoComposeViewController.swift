de9fd019750d272e9f09f00de2846de5a6e31ca4
c0407h <67133244+c0407h@users.noreply.github.com>
2023-12-20 23:18:28 +0900

    메모 쓰기화면을 구성하고 뷰 모델과 뷰 바인딩

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -6,8 +6,19 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+        
+    
     var viewModel: MemoComposeViewModel!
 
     override func viewDidLoad() {
@@ -16,8 +27,43 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
+    
     
     func bindViewModel() {
+        //네비게이션 타이틀 바인딩
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        
+        //이렇게 바인딩하면 메모 쓰ㅜ기에서는 빈문자열 편집에서는 편집할 메모가 표시됨
+        viewModel.initailText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+            
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        
+        //세이브 버튼틀 탭했을 때 contentTextView에 있는 최신 텍스트가 방출되고
+        //방출되는 텍스트가 saveAction에 있는 입력으로 바인딩됨
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
         
     }
     
